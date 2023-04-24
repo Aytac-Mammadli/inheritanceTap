6a5e3d6314a7c8bce5e13cdaa8981534c4e76877
Aytacc <aytacmammadli6@gmail.com>
2023-04-24 11:38:43 +0300

    Initial commit

---
+++ src/Human.java
@@ -0,0 +1,9 @@
+public class Human {
+    public String name;
+    public int id;
+
+    public Human(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
---
+++ src/Teacher.java
@@ -0,0 +1,9 @@
+public class Teacher  extends  Human{
+
+    public String pese;
+
+
+    public Teacher(String name, int id) {
+        super(name, id);
+    }
+}
